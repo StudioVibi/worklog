c56d3a30c0f5da3d77cbf71b9fd6e407ee562786
victortaelin <victor.taelin@gmail.com>
2026-02-04 15:16:18 -0300

    rename counter label

--- js/app.js
+++ js/app.js
@@ -218,7 +218,7 @@ const App = {
 
   updateCounter() {
     const displayMs = this.isAwaitingLog ? 60 * 60 * 1000 : this.elapsedMs;
-    this.elements.counter.textContent = `Pending: ${this.formatDuration(displayMs)}`;
+    this.elements.counter.textContent = `Worked: ${this.formatDuration(displayMs)}`;
   },
 
   togglePause() {
